459decdb85648b5c19e0471a3eada391ffd2f06e
vinicyusx <vinicyus3029@gmail.com>
2022-01-25 18:16:13 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,24 @@
+function mensagem(){
+    console.log("exibindo mesagem no console")
+}
+ mensagem("")
+
+
+ function ola(nome){
+     console.log(`${nome}`)
+ }
+
+ ola("Vinicyus")
+
+
+ function cantora(nome, idade, estilo){
+     console.log(`${nome}, ${idade}, ${estilo}`)
+ }
+
+ cantora("ludmilla", "26 anos", "canta funk carioca")
+
+ function filme(nome, musica){
+     console.log(`O melhor filme, ${nome}, amo quando eles cantam  ${musica}`)
+ }
+
+ filme("Mudança de habito", "Oh happy day")
